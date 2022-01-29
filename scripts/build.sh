0124d5da955e01deab817b487de9d05775a7f518
CzBiX <CzBiX@users.noreply.github.com>
2022-01-29 19:51:53 +0800

    Fix windows bulid filename

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 OUTPUT=${1:-"build/m3u8-parallel-downloader"}
+
+# append ext extension if not present
+if [ "$GOOS" = "windows" -a "${OUTPUT: -4}" != ".exe" ]; then
+    OUTPUT="${OUTPUT}.exe"
+fi
+
 echo "Building $OUTPUT"
 CGO_ENABLED=0 go build --trimpath --ldflags '-s -w' -o $OUTPUT
